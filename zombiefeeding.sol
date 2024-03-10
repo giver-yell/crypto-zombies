669d235437c6f8a7eeec29a01a6ab095734226fb
RyoheiFujihara-icp <ryohei.fujihara@infcurion.com>
2024-03-10 08:04:25 +0800

    ✨ ownable

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,10 +19,13 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species ) public {
+  function setKittyContractAddress(address _address) external {
+    kittyContract = KittyInterface(_address);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
